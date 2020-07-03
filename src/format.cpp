331aa8e80531e5d49df627af5a72ac89ff526b94
ieternalleo <jovaun.mcg@gmail.com>
2020-07-03 08:10:32 -0400

    Resolve std::length_error

--- src/format.cpp
+++ src/format.cpp
@@ -6,14 +6,22 @@ using std::string;
 using std::to_string;
 
 std::string Format::Pad(string s, char c) {
+
   s.insert(s.begin(), 2 - s.size(), c);
   return s;
 }
 
 string Format::Time(long int time) {
-  int hours = time / (60 * 60);
+
+  int hours = time / (60 * 60); 
+  
+  //Resolves std::length_error
+  //Shows
+  hours = (hours > 99) ? 99 : hours;
   int minutes = (time / 60) % 60;
   long seconds = time % 60;
+  
+  
   return std::string(Format::Pad(to_string(hours), '0') + ":" +
                      Format::Pad(to_string(minutes), '0') + ":" +
                      Format::Pad(to_string(seconds), '0'));
